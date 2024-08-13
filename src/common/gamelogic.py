98b769164342525d7762adc628e0aa461f1196d8
Hüseyin Sarsılmaz <huseyinsarsilmaz@hotmail.com>
2024-08-13 20:08:20 +0300

    Add deck sorting

--- src/common/gamelogic.py
+++ src/common/gamelogic.py
@@ -14,6 +14,7 @@ class GameStage(enum.Enum):
 WIDTH, HEIGHT = 1600, 900
 FPS = 60
 BGCOLOR = (2, 100, 42)
+SUIT_ORDER = {"H": 0, "S": 1, "D": 2, "C": 3}
 
 
 class Deck:
@@ -22,6 +23,9 @@ class Deck:
 
     def createDeck(self, cards: list):
         self.cards = cards
+
+    def sortDeck(self):
+        self.cards.sort(key=lambda x: (SUIT_ORDER[x.suit], x.rank))
 
 
 def dealCards(cards: list, decks: dict, myId: int):
@@ -36,23 +40,40 @@ def dealCards(cards: list, decks: dict, myId: int):
         for j in range(13):
             c = cards[j + i * 13]
             if i == myId:
-                c.xPos = 317 + j * 70
-                c.yPos = 668
-                c.visible = True
                 c.reverse = cardReverseVertical
+                c.visible = True
                 decks["my"].cards.append(c)
             elif i == (myId + 1) % 4:
-                c.xPos = 68
-                c.yPos = 88 + j * 50
                 c.reverse = cardReverseHorizontal
                 decks["left"].cards.append(c)
             elif i == (myId + 2) % 4:
-                c.xPos = 317 + j * 70
-                c.yPos = 50
                 c.reverse = cardReverseVertical
                 decks["mate"].cards.append(c)
             elif i == (myId + 3) % 4:
-                c.xPos = 1350
-                c.yPos = 88 + j * 50
                 c.reverse = cardReverseHorizontal
                 decks["right"].cards.append(c)
+
+    for deck in decks.values():
+        deck.sortDeck()
+
+    rePositionCards(decks)
+
+
+def rePositionCards(decks: dict):
+    for key, deck in decks.items():
+        if key == "my":
+            for i, card in enumerate(deck.cards):
+                card.xPos = 317 + i * 70
+                card.yPos = 668
+        elif key == "left":
+            for i, card in enumerate(deck.cards):
+                card.xPos = 68
+                card.yPos = 88 + i * 50
+        elif key == "mate":
+            for i, card in enumerate(deck.cards):
+                card.xPos = 317 + i * 70
+                card.yPos = 50
+        elif key == "right":
+            for i, card in enumerate(deck.cards):
+                card.xPos = 1350
+                card.yPos = 88 + i * 50
